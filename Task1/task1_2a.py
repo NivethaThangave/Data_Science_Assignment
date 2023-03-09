11657b1d49a5b847848f558978d213f956d3cd88
NivethaThangave <127142667+NivethaThangave@users.noreply.github.com>
2023-03-09 18:17:28 +0000

    Update task1_2a.py

--- Task1/task1_2a.py
+++ Task1/task1_2a.py
@@ -1,23 +1,23 @@
-def e(input_data):
-  b = ''
-  d = ''
+def encrypt_string(input_data):
+  output_data = ''
+  storing_data = ''
   count = 0
-  # Creating a for loop to iterate the given input string value from 0 to the length of the input string in-order to achieve all the input string data
+  # Creating a for loop to iterate the given input string value from 0 to the length of the input string in-order to achieve all the input string data.
   for i in range(1,len(input_data)-1):
-   # The below condition will be pass at the first run to calculate the first string count   
-   if (d==''):
+  # The below condition will be pass at the first run to calculate the first string count.
+   if (storing_data==''):
     count = 1
-   # The below condition will pass while comparing the input_data[0 to it's len] matches with the value stored in d, This will match only when the character repeats ; then  the count will get increamented to 1 
-   elif (input_data[i]==d):
+  # The below condition will pass while comparing the input_data[0 to it's len] matches with the value stored in d, This will match only when the character repeats ; then  the count will get increamented to 1.
+   elif (input_data[i]==storing_data):
     count = count+1 
    else:
-    # The below condition will pass when the above two condtions fails, or in the other words when d value is not empty and the iterated input data is not matching with the value stored in d (different character), then the count will be initialized to 1 for a new character. 
-    b = b+str(count)+d
+    # The below condition will pass when the above two condtions fails, or in the other words when d value is not empty and the iterated input data is not matching with the value stored in d (different character), then the count will be initialized to 1 for a new character.    
+    output_data = output_data+str(count)+storing_data
     count = 1
-   # storing each iteration value in d
-   d = input_data[i]
-  # The b will store the final result  
-  b = b+str(count)+d
-  return(b)
-output_Value = e("BBOO")
-print("Output Data : "+ output_Value)
+   # storing each iteration value in storing_data 
+   storing_data = input_data[i] 
+   # The output_data will store the final result  
+  output_data = output_data+str(count)+storing_data
+  return(output_data)
+outputData = encrypt_string("BBOO")
+print("Output Value : "+ outputData)
