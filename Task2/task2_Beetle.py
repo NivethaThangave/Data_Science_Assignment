60ea73a3cc5d05583abd0542b5f2e4af609fc8f5
NivethaThangave <127142667+NivethaThangave@users.noreply.github.com>
2023-03-10 05:59:01 +0000

    Update task2_Beetle.py

--- Task2/task2_Beetle.py
+++ Task2/task2_Beetle.py
@@ -5,52 +5,63 @@ def dice():
     will collect a parts of a Beetle and form it to a complete structure. Okay That’s interesting…but how 
     to know which part will be collected for which dice thrown? The following are the scenarios 
     to get the beetle part. The dice 1 will give a Beetle Body, 2 will give a Head , 
-    3 will give an Antenna, 4 will give an Eye, 5 will give a Mouth and 6 will give a leg.
+    3 will give an Antennaee , 4 will give an Eye, 5 will give a Mouth and 6 will give a leg.
     The Aim is to assemble a Beetle body which contains one Body, one Head, Two Antenna,
     Two Eyes, One Mouth and Six Legs.  But here are some rules to collect the beetle parts, 
     One cannot collect any parts directly and assemble a Beetle. The Body should be collected 
     first before any other parts and Head should be collected before collecting any Antennae,
     Eyes and Mouth.'''
-    BeetleBody=False
-    BeetleHead=False
-    BeetleAntenna=False
-    No_of_antenna=0
-    BeetleEyes=False
-    No_of_eyes=0
-    BeetleMouth=False
-    BeetleLegs=False
-    No_of_legs=0
-    Beetle = False
+    # Beetle parts initialization
+    btleBody=False
+    btleHead=False
+    btleAntennaee =False
+    no_of_antennaee=0
+    btleEyes=False
+    no_of_eyes=0
+    btleMouth=False
+    btleLegs=False
+    no_of_legs=0
+    beetle = False
     print("START THE GAME by rolling the DICE...")
     print("The complete Beetle structure will be assembled by the below dice rules :")
-    for n in range(0,200):
+    # Throwing the dice from o to 200 to assemble the beetle and it will come automatically out of the loop if it assembles a beetle body before 200 counts
+    for n in range(0,100):
+        # using random.randint(1,6) to get the integer vakue randomly from 1 to 6 
         dice_thrown=random.randint(1,6)
-        if(dice_thrown==1 and BeetleBody==False):
-            BeetleBody=True
+        # when throwing dice 1 to get a body
+        if(dice_thrown==1 and btleBody==False):
+            btleBody=True
             print("When throwing dice for " +str(n)+" th time, we got dice " + str(dice_thrown)+" Hence the Body got assembled for Beetle")
-        elif(dice_thrown==2 and BeetleHead==False and BeetleBody==True):
-            BeetleHead=True
+        # when throwing dice 2 to get a head
+        elif(dice_thrown==2 and btleHead==False and btleBody==True):
+            btleHead=True
             print("When throwing dice for " +str(n)+" th time, we got dice " + str(dice_thrown)+" Hence the Head got assembled for Beetle")
-        elif(dice_thrown==3 and BeetleHead==True and BeetleAntenna==False and No_of_antenna<2):
-            No_of_antenna=No_of_antenna+1
-            print("When throwing dice for " +str(n)+" th time, we got dice " + str(dice_thrown)+" Hence the antenna no " + str(No_of_antenna) + " got assembled for Beetle")
-            if(No_of_antenna==2):
-                BeetleAntenna=True
-        elif(dice_thrown==4 and BeetleHead==True and BeetleEyes==False and No_of_eyes<2):
-            No_of_eyes=No_of_eyes+1
-            print("When throwing dice for "+str(n)+" th time, we got dice " +str(dice_thrown)+" Hence the of eye " + str(No_of_eyes) + " got assembled for Beetle")
-            if(No_of_eyes==2):
-                BeetleEyes=True
-        elif(dice_thrown==5 and BeetleHead==True and BeetleMouth==False):
-            BeetleMouth=True
+        # when throwing dice 3 to get an anennaee
+        elif(dice_thrown==3 and btleHead==True and btleAntennaee ==False and no_of_antennaee<2):
+            no_of_antennaee=no_of_antennaee+1
+            print("When throwing dice for " +str(n)+" th time, we got dice " + str(dice_thrown)+" Hence the antenna no " + str(no_of_antennaee) + " got assembled for Beetle")
+            if(no_of_antennaee==2):
+                btleAntennaee=True
+        #when thriwing dice 4 to get eyes
+        elif(dice_thrown==4 and btleHead==True and btleEyes==False and no_of_eyes<2):
+            no_of_eyes=no_of_eyes+1
+            print("When throwing dice for "+str(n)+" th time, we got dice " +str(dice_thrown)+" Hence the of eye " + str(no_of_eyes) + " got assembled for Beetle")
+            if(no_of_eyes==2):
+                btleEyes=True
+        # when throwing dice 5 to get a mouth        
+        elif(dice_thrown==5 and btleHead==True and btleMouth==False):
+            btleMouth=True
             print("When throwing dice for " +str(n)+" th time, we got dice " + str(dice_thrown)+" Hence the Mouth got assembled for Beetle")
-        elif(dice_thrown==6 and BeetleBody==True and BeetleLegs==False and No_of_legs<6):
-            No_of_legs=No_of_legs+1
-            print("when throwing dice for " +str(n)+" th time, we got dice " +str(dice_thrown)+ " Hence the leg no " +str(No_of_legs) +" got assembled for Beetle")
-            if(No_of_legs==6):
-                BeetleLegs=True
-        elif(BeetleBody==True and BeetleHead==True and BeetleAntenna==True and BeetleEyes==True and BeetleMouth==True and BeetleLegs==True and Beetle==False):
-                print(" Congratulations!!!! You have Collected all the parts of the Beetle and assembled it.. The overall move is " + str(n))
-                Beetle=True
+        # when throwing dice 6 to get a leg
+        elif(dice_thrown==6 and btleBody==True and btleLegs==False and no_of_legs<6):
+            no_of_legs=no_of_legs+1
+            print("when throwing dice for " +str(n)+" th time, we got dice " +str(dice_thrown)+ " Hence the leg no " +str(no_of_legs) +" got assembled for Beetle")
+            if(no_of_legs==6):
+                btleLegs=True
+        # to check all the parts got assembled and to complete the game        
+        elif(btleBody==True and btleHead==True and btleAntennaee==True and btleEyes==True and btleMouth==True and btleLegs==True and beetle==False):
+                print(" Congradulations!!!! You have Collected all the parts of the Beetle and assembled it.. The overall move is " + str(n))
+                beetle=True
     return("Hence the game is Over.!!!!!")
 print(dice())
+
